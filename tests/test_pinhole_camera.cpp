525cc67d3b283dc069597899e866b76277c819f5
okn-yu <test@test.com>
2022-05-30 23:09:19 +0900

    updated.

--- tests/test_pinhole_camera.cpp
+++ tests/test_pinhole_camera.cpp
@@ -1,13 +1,33 @@
-//
-// Created by okn-yu on 2022/05/06.
-//
+/*
+ * Created by okn-yu on 2022/05/06.
+ */
 #include <gtest/gtest.h>
 #include "vec3.hpp"
-#include "ray.hpp"
 #include "image.hpp"
 #include "pinhole_camera.h"
 
-TEST(PINHOLE_CAMERA_TEST, PNG){
+// 慣例とは逆にカメラの視線方向は(0, 0, 1)
+TEST(PINHOLE_CAMERA_TEST, VEC_TEST1){
+    Image<RGBPixel> img(256, 256);
+    PinholeCamera cam(Vec3(0, 0, 0), Vec3(0, 0, 1), 1);
+
+    EXPECT_EQ(cam.cam_right_vec, Vec3(1, 0, 0));
+    EXPECT_EQ(cam.cam_up_vec, Vec3(0, 1, 0));
+}
+
+// 慣例と同じくカメラの視線方向は(0, 0, -1)
+// cam_right_vecが(-1, 0, 0)とり、cam_upは変わらない
+// つまりy軸を中心にして反時計周りに180度回転したことに相当する
+// ここはいまいちしっくりと来ない
+TEST(PINHOLE_CAMERA_TEST, VEC_TEST2){
+    Image<RGBPixel> img(256, 256);
+    PinholeCamera cam(Vec3(0, 0, 0), Vec3(0, 0, -1), 1);
+
+    EXPECT_EQ(cam.cam_right_vec, Vec3(-1, 0, 0));
+    EXPECT_EQ(cam.cam_up_vec, Vec3(0, 1, 0));
+}
+
+/*TEST(PINHOLE_CAMERA_TEST, PNG){
     Image<RGBPixel> img(256, 256);
     PinholeCamera cam(Vec3(0, 0, 0), Vec3(0, 0, -1), 1);
 
@@ -26,4 +46,4 @@ TEST(PINHOLE_CAMERA_TEST, PNG){
     }
     img.png_output("pinhole_test.png", 3);
 
-}
+}*/
