4f4d7b5441088bafcc76eb7f979576c83f108d31
okn-yu <test@test.com>
2022-07-23 21:45:12 +0900

    updated.

---
+++ src/futaba/futaba.cpp
@@ -0,0 +1,17 @@
+//
+// Created by okn-yu on 2022/05/06.
+//
+
+#include <iostream>
+#include <cstdint>
+#include "core/vec3.h"
+
+using namespace std;
+
+int main() {
+    std::cout << "futaba called!" << std::endl;
+
+}
+
+
+
